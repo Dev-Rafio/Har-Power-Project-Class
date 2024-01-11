3c4f3c1bed46ef3c89287e3b898f4bffb87d451c
dev-rafio <rafiomusic@gmail.com>
2024-01-11 11:58:07 +0600

    some add

---
+++ javascript class/script.js
@@ -0,0 +1,13 @@
+
+// One of many JavaScript HTML methods is getElementById().
+function print() {
+    document.getElementById('cng').innerHTML='hello'
+}
+
+
+// JavaScript Can Change HTML Styles (CSS)
+function size() {
+    document.getElementById('font').style.fontSize="30px"
+    document.getElementById('font').style.color="red"
+}
+
