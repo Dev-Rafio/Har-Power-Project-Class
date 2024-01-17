57d0bbd658e78dcd457848c2aa32633e15b15c70
dev-rafio <rafiomusic@gmail.com>
2024-01-17 11:24:43 +0600

    some code add

--- javascript class/js-function/script.js
+++ javascript class/js-function/script.js
@@ -3,16 +3,18 @@
 
 // function create
 function sleep() {
-    console.log("Kabir is sleeping form 10pm");
+    console.log("Kabir is sleeping from 10pm");
 }
 sleep()
 
+
 // function parameter
 function sleeps(names) {
-    console.log(names +" is sleeping form 11pm");
+    console.log(names +" is sleeping from 11pm");
 }
 sleeps("Rafio")
 sleeps("Kabir")
+sleeps("rumi")
 
 // function  multiple parameter
 function slepp(names,tyme) {
@@ -23,7 +25,65 @@ slepp("Rafio","11pm")
 
 // Function Return
 function myfunction(a,b) {
-    return(a*b)
+    console.log(a*b);
+   
 }
-let x=myfunction(3,3)
-console.log(x);
+myfunction(3,4)
+
+
+
+
+
+// ====================================================================
+// try it
+
+function printName(firstName, lastName ){
+    console.log(firstName+" "+lastName);
+
+}
+
+printName("Rafio","Kabir") //function argument
+printName("kabir", "rafio")
+
+// veriable add 
+function printNam(firstName, lastName ){
+    console.log(firstName+" "+lastName);
+
+}
+var first='Rumi';
+var last='prokrite';
+
+printNam(first, last)
+
+//user input 
+
+
+function printNa(firstName, lastName){
+    console.log(firstName+' '+lastName);
+    
+}
+let firstName=prompt("Enter Your First Name")
+    let lastName=prompt("Enter Your last Name")
+printNa(firstName, lastName)
+
+
+
+//sum veriable
+function sum1(firstNum,lastNum){
+    console.log(firstNum + lastNum);
+}
+let firs=+1;
+let las=+2;
+sum1(firs,las)
+
+
+//sum input
+function sum(firstNum, lastNum){
+    console.log(firstNum+lastNum)
+
+}
+
+let firstNum=+prompt("Enter first number");
+let lastNum=+prompt("Enter last number");
+
+sum(firstNum,lastNum)
---
+++ javascript class/js-object/script.js
@@ -0,0 +1,11 @@
+// JavaScript Objects
+let car={
+    name:"bmw",
+    color:"black",
+    model:5000,
+    weight:"180kg",
+
+    start: function(){
+        console.log("The Smart  car");
+    }
+}
